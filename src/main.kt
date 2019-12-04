c75f436ecbb8d2e01c101a9a2e24dbca461eeb06
Seunghoon Oh <stdsalmon@gmail.com>
2019-12-05 00:07:28 +0900

    Add example and kr comment

--- src/main.kt
+++ src/main.kt
@@ -1,31 +1,35 @@
+data class Person(var age: Int = 0, var name: String = "java") {
+}
+
 object V1 {
-    fun buildString(
-        buildAction: (StringBuilder) -> Unit
-    ): String {
-        val sb = StringBuilder()
-        buildAction(sb)
-        return sb.toString()
+    fun makePerson(
+        makePersonAction: (Person) -> Unit
+    ): Person {
+        val person = Person()
+        makePersonAction(person)
+        return person
     }
 }
 
 object V2 {
-    fun buildString(
-        buildAction: StringBuilder.() -> Unit
-    ): String {
-        val sb = StringBuilder()
-        buildAction(sb)
-        return sb.toString()
+    fun makePerson(
+        makePersonAction: Person.() -> Unit
+    ): Person {
+        val person = Person()
+        person.makePersonAction()
+        return person
     }
 }
 
 fun main() {
-    println(V1.buildString {
-        it.append("HELLO")
-        it.append("DSL")
+    println(V1.makePerson {
+        it.age = 10
+        it.name = "kotlin"
     })
 
-    println(V2.buildString {
-        append("V2")
-        append("DSL")
+    println(V2.makePerson {
+        //확장함수처럼 this가 넘어가기 때문에 it을 붙이지 않아도 된다
+        age = 10
+        name = "kotlin"
     })
 }
